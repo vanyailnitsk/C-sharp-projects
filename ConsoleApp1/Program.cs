46fdb41fad5d367d0f7a9259d9bc65b5f735c962
vanyailnitsk <vanyailnitsk@yandex.ru>
2022-05-08 15:54:42 +0300

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp1
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "5,33";
+            try
+            {
+                double a = double.Parse(str);
+                Console.WriteLine(a + "\nУспешная конвертация!");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Ошибка при конвертации!");
+            }
+
+         }
+    }
+
+}
